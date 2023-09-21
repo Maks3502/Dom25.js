575dc17b7d51bbee5f38f337aefcbdd217349910
Maks3502 <107855225+Maks3502@users.noreply.github.com>
2023-09-21 18:03:24 +0300

    Add files via upload

---
+++ Dom25.js
@@ -0,0 +1,52 @@
+// завдання 2
+
+// const ingredients = [
+//     'Картопля',
+//     'Гриби',
+//     'Часник',
+//     'Помідори',
+//     'Зелень',
+//     'Приправи',
+//   ];
+
+
+
+
+//   const list = document.querySelector(".list_product");
+
+//   const productItem = ingredients.map((products) => `<li class="list_item">${products}</li>`).join();
+//   console.log(productItem);
+
+//   list.append(productItem);
+//   console.log(list);
+
+//   list.innerHTML = productItem;
+
+// завдання 1 
+// const animalHome = ["Кіт", "Хом'як", "Кінь", "Папуга"];
+// const listAnimal = document.querySelector(".list-animal");
+// const animalItem = animalHome.map((animals) => `<li class = "item-animal"><h2 class="name-animal">${animals}</h2></li>`).join();
+// console.log(animalItem)
+// listAnimal.append(animalItem);
+// console.log(listAnimal)
+// listAnimal.innerHTML = animalItem;
+
+// const productMarket = ["Хліб", "Петрушка", "Сир"];
+// const listProduct = document.querySelector(".list-product");
+// const productlItem = productMarket.map((products) => `<li class = "item-product"><h2 class="name-product">${products}</h2></li>`).join();
+// console.log(productlItem);
+// listProduct.append(productlItem);
+// console.log(listProduct)
+// listProduct.innerHTML = productlItem;
+
+// const itTehnology = ["HTML", "CSS", "JavaScript", "React", "Node"];
+// const listTehnology = document.querySelector(".list-tehnology");
+// const tehnologyItem = itTehnology.map((Tehnologys) => `<li class = "item-tehno"><h2 class="name-tehno">${Tehnologys}</h2></li>`).join();
+// console.log(tehnologyItem);
+// listTehnology.append(tehnologyItem);
+// console.log(listTehnology)
+// listTehnology.innerHTML = tehnologyItem;
+
+
+
+
